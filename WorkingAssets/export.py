e3328b688c902441af19edcbb43988cbd853c798
Thomas Bartlett <tbartl@umich.edu>
2025-02-11 22:35:17 -0500

    Accepting that I can't separate these into separate FBXs and just putting them all into one giga one

--- WorkingAssets/export.py
+++ WorkingAssets/export.py
@@ -7,8 +7,18 @@ def export():
     
     for selectedObject in selectedObjects:
         selectedObject.select_set(False)
+        
+    exportObjects = []
+    for obj in selectedObjects:
+        # Never export a mesh without its parent
+        if obj.type == 'MESH' and obj.parent and obj.parent.type == 'ARMATURE':
+            exportObjects.append(obj.parent)
+        else:
+            exportObjects.append(obj)
+    # Make unique
+    exportObjects = list(set(exportObjects)) 
     
-    for selectedObject in selectedObjects:
+    for selectedObject in exportObjects:
         exportPath = os.path.dirname(bpy.data.filepath)
         exportPath += "/../Assets/Art/Models/"
 
@@ -27,12 +37,28 @@ def export():
         exportPath += ".fbx"
         exportPath = os.path.abspath(exportPath)
         
+        select_children_recursive(selectedObject, True)
+        bpy.ops.export_scene.fbx(
+            filepath = exportPath,
+            use_selection = True,
+            axis_forward = 'Z',
+            axis_up = 'Y',
+            bake_space_transform=True,
+            apply_scale_options='FBX_SCALE_ALL',
+            use_mesh_modifiers=True,  # Ensure the final deformed shape is exported
+            object_types={'MESH', 'ARMATURE'},  # Include only mesh and armature
+            use_armature_deform_only=True,  # Export only bones used for deformation
+            add_leaf_bones=False,  # Prevents extra bones from being added
+        )
+        select_children_recursive(selectedObject, False)
         
-        selectedObject.select_set(True)
-        bpy.ops.export_scene.fbx(filepath = exportPath, use_selection = True, axis_forward = 'Z', axis_up = 'Y', bake_space_transform=True, apply_scale_options='FBX_SCALE_ALL')
-        selectedObject.select_set(False)
     
     for selectedObject in selectedObjects:
         selectedObject.select_set(True)
+        
+def select_children_recursive(obj, doSelect):
+    obj.select_set(doSelect)
+    for child in obj.children:
+        select_children_recursive(child, doSelect)
     
 export()
