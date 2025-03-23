28bbd2d76e75b868b30d691e0b4f2554ac7dee81
Thomas Bartlett <tbartl@umich.edu>
2025-03-22 23:19:53 -0400

    Final animation cleanup

--- Assets/Scripts/Misc/TestDemos/DemoCameraPan.cs
+++ Assets/Scripts/Misc/TestDemos/DemoCameraPan.cs
@@ -4,7 +4,12 @@ public class DemoCameraPan : MonoBehaviour
 {
     [SerializeField] AnimationCurve CURVE;
     [SerializeField] float SPEED;
+    void Start()
+    {
 
+
+        Screen.SetResolution(1200, 1200, FullScreenMode.Windowed);
+    }
     void Update()
     {
         this.transform.rotation = Quaternion.Euler(0, -CURVE.Evaluate(SPEED * Time.time) * 360, 0);
