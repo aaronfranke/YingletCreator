d6a9b5af8700389b47c739ab28f8a01ac357dc8d
Thomas Bartlett <tbartl@umich.edu>
2025-04-02 01:24:35 -0400

    Play sound on textbox letter

---
+++ Assets/Scripts/Audio/PlaySoundOnTextboxLetter.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class PlaySoundOnTextboxLetter : MonoBehaviour
+{
+    [SerializeField] private SoundEffect _soundEffect;
+    private IAudioPlayer _audioPlayer;
+    private TMP_InputField _inputField;
+
+    private void Awake()
+    {
+        _audioPlayer = Singletons.GetSingleton<IAudioPlayer>();
+        _inputField = this.GetComponent<TMP_InputField>();
+        _inputField.onValueChanged.AddListener(InputField_OnValueChanged);
+    }
+
+    private void OnDestroy()
+    {
+        _inputField.onValueChanged.RemoveListener(InputField_OnValueChanged);
+    }
+
+    private void InputField_OnValueChanged(string arg0)
+    {
+        _audioPlayer.Play(_soundEffect);
+    }
+}
