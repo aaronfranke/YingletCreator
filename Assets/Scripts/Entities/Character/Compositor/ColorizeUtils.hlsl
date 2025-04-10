25926fb11e7f890f342b2e516a99fcea2c6c572b
Thomas Bartlett <tbartl@umich.edu>
2025-04-09 22:35:05 -0400

    Rewrite colorize shaders

--- Assets/Scripts/Entities/Character/Compositor/ColorizeUtils.hlsl
+++ Assets/Scripts/Entities/Character/Compositor/ColorizeUtils.hlsl
@@ -1,8 +1,35 @@
 #ifndef COLORIZE_UTILS_INCLUDED
 #define COLORIZE_UTILS_INCLUDED
 
+float saturateAndGammaConvert(float f)
+{
+                // Saturating alone will cause the resulting black/white ranges to look like ass
+                // So convert it from rgb to gamma
+    f = saturate(f);
+    f = pow(f, 2.2f); // Good enough heuristic
+    return f;
+}
+
+float4 GrayscaleToColored(float4 mainTexColor, float4 grayscaleColor, float4 color, float4 darkColor)
+{
+    float range25to75 = (grayscaleColor.r - 0.25f) * 2;
+    float3 colorizedColor = lerp(darkColor.rgb, color.rgb, saturate(range25to75));
+
+    float range0to25 = grayscaleColor.r * 4;
+    colorizedColor = lerp((0, 0, 0), colorizedColor, saturateAndGammaConvert(range0to25));
+
+    float range75to100 = (grayscaleColor.r - 0.75f) * 4;
+    colorizedColor = lerp(colorizedColor, (1, 1, 1), saturateAndGammaConvert(range75to100));
+
+    float4 outColor = mainTexColor;
+    outColor.rgb = lerp(mainTexColor.rgb, colorizedColor, grayscaleColor.a);
+    outColor.a = max(mainTexColor.a, grayscaleColor.a);
+    return outColor;
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////
-//  Borrowed from unity
+//  Everything below this line is from old atlyss-style color shifting logic that I may want to return for specific things
 ////////////////////////////////////////////////////////////////////////////////////////////
 void Unity_Hue_Degrees_float(float3 In, float Offset, out float3 Out)
 {
