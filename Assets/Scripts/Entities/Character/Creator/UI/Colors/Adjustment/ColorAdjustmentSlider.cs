610c4a730499bf308259f3db9ae6c24ed82bbc5a
Thomas Bartlett <tbartl@umich.edu>
2025-04-14 17:13:20 -0400

    Saturation / Value sliders

--- Assets/Scripts/Entities/Character/Creator/UI/Colors/Adjustment/ColorAdjustmentSlider.cs
+++ Assets/Scripts/Entities/Character/Creator/UI/Colors/Adjustment/ColorAdjustmentSlider.cs
@@ -2,13 +2,17 @@ using Assets.Scripts.Entities.Character.Compositor;
 using Character.Compositor;
 using Character.Data;
 using Reactivity;
+using System;
 using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Character.Creator.UI
 {
 	public class ColorAdjustmentSlider : ReactiveBehaviour
 	{
+		[SerializeField] ColorAdjustmentSliderTarget _target;
+
 		private IColorActiveSelection _activeSelection;
 		private ICustomizationSelectedDataRepository _dataRepo;
 		private Slider _slider;
@@ -36,7 +40,7 @@ namespace Character.Creator.UI
 		{
 			var id = _activeSelection.FirstSelected;
 			if (!id) return;
-			_slider.SetValueWithoutNotify(_dataRepo.GetColorizeValues(id).Base.Hue);
+			_slider.SetValueWithoutNotify(GetTargetVal(_dataRepo.GetColorizeValues(id).Base));
 		}
 
 
@@ -48,14 +52,14 @@ namespace Character.Creator.UI
 			var existingColors = ids.Select(id => new IdWithColor(id, _dataRepo.GetColorizeValues(id))).ToList();
 
 			var firstColor = existingColors.First();
-			var diff = value - firstColor.ColorizeValues.Base.Hue;
+			var diff = value - GetTargetVal(firstColor.ColorizeValues.Base);
 
 			using var suspender = new ReactivitySuspender();
 			foreach (var color in existingColors)
 			{
 				var writeableColor = new WriteableColorizeValues(color.ColorizeValues);
-				writeableColor.Base.Hue += diff;
-				writeableColor.Shade.Hue += diff;
+				AdjustTargetVal(writeableColor.Base, diff);
+				AdjustTargetVal(writeableColor.Shade, diff);
 				_dataRepo.SetColorizeValues(color.Id, writeableColor);
 			}
 		}
@@ -69,5 +73,28 @@ namespace Character.Creator.UI
 			public ReColorId Id { get; }
 			public IColorizeValues ColorizeValues { get; }
 		}
+
+		float GetTargetVal(IColorizeValuesPart part)
+		{
+			if (_target == ColorAdjustmentSliderTarget.Hue) return part.Hue;
+			else if (_target == ColorAdjustmentSliderTarget.Saturation) return part.Saturation;
+			else if (_target == ColorAdjustmentSliderTarget.Value) return part.Value;
+			else throw new ArgumentException("Invalid target");
+		}
+
+		void AdjustTargetVal(WriteableColorizeValuesPart part, float diff)
+		{
+			if (_target == ColorAdjustmentSliderTarget.Hue) part.Hue += diff;
+			else if (_target == ColorAdjustmentSliderTarget.Saturation) part.Saturation += diff;
+			else if (_target == ColorAdjustmentSliderTarget.Value) part.Value += diff;
+			else throw new ArgumentException("Invalid target");
+		}
+	}
+
+	public enum ColorAdjustmentSliderTarget
+	{
+		Hue,
+		Saturation,
+		Value
 	}
 }
