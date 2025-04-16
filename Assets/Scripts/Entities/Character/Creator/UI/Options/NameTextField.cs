d6fbfb2bbe7e378f83bb27c5d372c58d0e7c708c
Thomas Bartlett <tbartl@umich.edu>
2025-04-16 19:49:32 -0400

    Text is not interactable for presets

--- Assets/Scripts/Entities/Character/Creator/UI/Options/NameTextField.cs
+++ Assets/Scripts/Entities/Character/Creator/UI/Options/NameTextField.cs
@@ -3,37 +3,52 @@ using TMPro;
 
 namespace Character.Creator.UI
 {
-    public class NameTextField : ReactiveBehaviour
-    {
-        private ICustomizationSelectedDataRepository _dataRepository;
-        private TMP_InputField _inputField;
+	public class NameTextField : ReactiveBehaviour
+	{
+		private ICustomizationSelectedDataRepository _dataRepository;
+		private ICustomizationSelection _selection;
+		private TMP_InputField _inputField;
 
-        private void Awake()
-        {
-            _dataRepository = this.GetComponentInParent<ICustomizationSelectedDataRepository>();
-            _inputField = this.GetComponent<TMP_InputField>();
-            _inputField.onValueChanged.AddListener(InputField_OnValueChanged);
-        }
+		private void Awake()
+		{
+			_dataRepository = this.GetComponentInParent<ICustomizationSelectedDataRepository>();
+			_selection = this.GetComponentInParent<ICustomizationSelection>();
+			_inputField = this.GetComponent<TMP_InputField>();
+			_inputField.onValueChanged.AddListener(InputField_OnValueChanged);
+		}
 
-        private void Start()
-        {
-            AddReflector(ReflectText);
-        }
+		private void Start()
+		{
+			AddReflector(ReflectText);
+			AddReflector(ReflectInteractable);
+		}
 
-        void ReflectText()
-        {
-            _inputField.text = _dataRepository.CustomizationData.Name.Val;
-        }
 
-        private new void OnDestroy()
-        {
-            base.OnDestroy();
-            _inputField.onValueChanged.RemoveListener(InputField_OnValueChanged);
-        }
+		void ReflectText()
+		{
+			_inputField.text = _dataRepository.CustomizationData.Name.Val;
+		}
 
-        private void InputField_OnValueChanged(string arg0)
-        {
-            _dataRepository.CustomizationData.Name.Val = arg0;
-        }
-    }
+		private void ReflectInteractable()
+		{
+			var selected = _selection.Selected;
+			if (selected == null)
+			{
+				_inputField.interactable = false;
+			}
+			_inputField.interactable = _selection.Selected.Group == CustomizationYingletGroup.Custom;
+		}
+
+
+		private new void OnDestroy()
+		{
+			base.OnDestroy();
+			_inputField.onValueChanged.RemoveListener(InputField_OnValueChanged);
+		}
+
+		private void InputField_OnValueChanged(string arg0)
+		{
+			_dataRepository.CustomizationData.Name.Val = arg0;
+		}
+	}
 }
