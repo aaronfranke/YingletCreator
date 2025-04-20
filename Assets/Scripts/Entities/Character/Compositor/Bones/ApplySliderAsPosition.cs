55678e94879daed2303297a990f2741a461473f6
Thomas Bartlett <tbartl@umich.edu>
2025-04-20 14:06:32 -0400

    Nose bridge slider: affects position

---
+++ Assets/Scripts/Entities/Character/Compositor/Bones/ApplySliderAsPosition.cs
@@ -0,0 +1,43 @@
+using Character.Creator;
+using Character.Data;
+using UnityEngine;
+
+
+public class ApplySliderAsPosition : MonoBehaviour, IApplyableCustomization
+{
+	[SerializeField] CharacterSliderId _sliderId;
+	[SerializeField] Transform _target;
+	[SerializeField] Vector3 _minOffset = Vector3.zero;
+	[SerializeField] Vector3 _maxOffset = Vector3.zero;
+
+	ICustomizationSelectedDataRepository _dataRepository;
+
+	private void Awake()
+	{
+		_dataRepository = GetComponentInParent<ICustomizationSelectedDataRepository>();
+	}
+
+	public void Apply()
+	{
+		var value = GetOffset();
+		_target.transform.position += _target.transform.TransformVector(value);
+
+	}
+
+	Vector3 GetOffset()
+	{
+		var sliderValue = _dataRepository.GetSliderValue(_sliderId);
+		var middle = 0.5f;
+		if (sliderValue < middle)
+		{
+			float p = sliderValue / middle;
+			return Vector3.LerpUnclamped(_minOffset, Vector3.zero, p);
+		}
+		else
+		{
+			float p = (sliderValue - middle) / (1 - middle);
+			return Vector3.LerpUnclamped(Vector3.zero, _maxOffset, p);
+		}
+	}
+
+}
