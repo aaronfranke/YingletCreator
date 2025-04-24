700ae0ba382e8747ebe83c3952ef2a22299748ee
Thomas Bartlett <tbartl@umich.edu>
2025-04-23 20:24:13 -0400

    Functional toggle selection

--- Assets/Scripts/Entities/Character/Creator/UI/MainPage/YingPortraits/YingPortraitReference.cs
+++ Assets/Scripts/Entities/Character/Creator/UI/MainPage/YingPortraits/YingPortraitReference.cs
@@ -2,35 +2,35 @@ using Reactivity;
 
 namespace Character.Creator.UI
 {
-    public interface IYingPortraitReference : ISelectable
-    {
-        void Setup(CachedYingletReference reference);
-        public CachedYingletReference Reference { get; }
-    }
+	public interface IYingPortraitReference : ISelectable
+	{
+		void Setup(CachedYingletReference reference);
+		public CachedYingletReference Reference { get; }
+	}
 
-    public class YingPortraitReference : ReactiveBehaviour, IYingPortraitReference
-    {
-        private ICustomizationSelection _selection;
-        private Computed<bool> _selected;
-        private Observable<CachedYingletReference> _reference = new();
+	public class YingPortraitReference : ReactiveBehaviour, IYingPortraitReference
+	{
+		private ICustomizationSelection _selection;
+		private Computed<bool> _selected;
+		private Observable<CachedYingletReference> _reference = new();
 
-        public CachedYingletReference Reference => _reference.Val;
-        public IReadOnlyObservable<bool> Selected => _selected;
+		public CachedYingletReference Reference => _reference.Val;
+		public IReadOnlyObservable<bool> Selected => _selected;
 
-        void Awake()
-        {
-            _selection = this.GetComponentInParent<ICustomizationSelection>();
-            _selected = CreateComputed(ComputeSelected);
-        }
+		void Awake()
+		{
+			_selection = this.GetComponentInParent<ICustomizationSelection>();
+			_selected = CreateComputed(ComputeSelected);
+		}
 
-        public void Setup(CachedYingletReference reference)
-        {
-            _reference.Val = reference;
-        }
+		public void Setup(CachedYingletReference reference)
+		{
+			_reference.Val = reference;
+		}
 
-        bool ComputeSelected()
-        {
-            return _reference.Val == _selection.Selected;
-        }
-    }
+		bool ComputeSelected()
+		{
+			return _reference.Val == _selection.Selected;
+		}
+	}
 }
---
+++ Assets/Scripts/Entities/Character/Creator/UI/Options/Toggle/CharacterCreatorToggleSelection.cs
@@ -0,0 +1,33 @@
+using Character.Data;
+using Reactivity;
+
+namespace Character.Creator.UI
+{
+	public class CharacterCreatorToggleSelection : ReactiveBehaviour, ISelectable
+	{
+		public CharacterToggleId ToggleId { get; set; }
+
+		Computed<bool> _selected;
+		private ICustomizationSelectedDataRepository _dataRepo;
+		private ICharacterCreatorToggleReference _reference;
+
+		public IReadOnlyObservable<bool> Selected => _selected;
+
+		void Awake()
+		{
+			_dataRepo = this.GetComponentInParent<ICustomizationSelectedDataRepository>();
+			_reference = this.GetComponent<ICharacterCreatorToggleReference>();
+		}
+
+		void Start()
+		{
+			_selected = CreateComputed(ComputeSelected);
+		}
+
+		private bool ComputeSelected()
+		{
+			var toggleVal = _dataRepo.GetToggle(_reference.ToggleId);
+			return toggleVal;
+		}
+	}
+}
