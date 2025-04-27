ba712592404d00db34fd9b115b50798cd9d286d1
Thomas Bartlett <tbartl@umich.edu>
2025-04-27 01:10:28 -0400

    Fix eye look at failing after base rotation

--- Assets/Scripts/Entities/Animation/Eye/EyeLookAt.cs
+++ Assets/Scripts/Entities/Animation/Eye/EyeLookAt.cs
@@ -7,32 +7,38 @@ using UnityEngine;
 /// </summary>
 public class EyeLookAt : MonoBehaviour, IEyeOffsetProvider
 {
-    [SerializeField] Transform _headBone;
-    [SerializeField] float _degreesToOffsetMult = .1f;
+	[SerializeField] Transform _headBone;
+	[SerializeField] float _degreesToOffsetMult = .1f;
+	private Transform _root;
+	Vector3 _originalForward;
 
-    Vector3 _originalForward;
+	void Awake()
+	{
+		_root = this.GetComponentInParent<YingletVisualsRoot>().transform;
+		_originalForward = GetHeadForward();
+	}
 
-    void Awake()
-    {
-        _originalForward = _headBone.forward;
-    }
+	private Vector3 GetHeadForward()
+	{
+		return _root.InverseTransformVector(_headBone.forward);
+	}
 
-    public Vector2 Offset
-    {
-        get
-        {
-            var angle = GetYRotation(_originalForward, _headBone.forward);
-            if (angle > 180) angle -= 360;
+	public Vector2 Offset
+	{
+		get
+		{
+			var angle = GetYRotation(_originalForward, GetHeadForward());
+			if (angle > 180) angle -= 360;
 
-            return new Vector2(angle * _degreesToOffsetMult, 0);
-        }
-    }
-    public static float GetYRotation(Vector3 from, Vector3 to)
-    {
-        // Get the angle in radians between the two vectors on the XY plane
-        float angle = Mathf.Atan2(to.x, to.z) - Mathf.Atan2(from.x, from.z);
+			return new Vector2(angle * _degreesToOffsetMult, 0);
+		}
+	}
+	public static float GetYRotation(Vector3 from, Vector3 to)
+	{
+		// Get the angle in radians between the two vectors on the XY plane
+		float angle = Mathf.Atan2(to.x, to.z) - Mathf.Atan2(from.x, from.z);
 
-        // Convert to degrees
-        return Mathf.Rad2Deg * angle;
-    }
+		// Convert to degrees
+		return Mathf.Rad2Deg * angle;
+	}
 }
