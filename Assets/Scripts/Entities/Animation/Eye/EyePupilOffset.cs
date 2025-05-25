6c578da534ec17b64de3f5694cbbe9f3a9c32c8c
Thomas Bartlett <tbartl@umich.edu>
2025-05-25 16:00:33 -0400

    Fix pupil offset to work with newly centered pupil

--- Assets/Scripts/Entities/Animation/Eye/EyePupilOffset.cs
+++ Assets/Scripts/Entities/Animation/Eye/EyePupilOffset.cs
@@ -3,39 +3,45 @@ using UnityEngine;
 
 public interface IEyeOffsetProvider
 {
-    Vector2 Offset { get; }
-    bool enabled { get; }
+	Vector2 Offset { get; }
+	bool enabled { get; }
 }
 
 public class EyePupilOffset : MonoBehaviour
 {
-    private IEyeGatherer _eyeGatherer;
-    private IEyeOffsetProvider[] _offsetProviders;
-    static readonly int PUPIL_OFFSET_X_PROP_ID = Shader.PropertyToID("_PupilOffsetX");
-    static readonly int PUPIL_OFFSET_Y_PROP_ID = Shader.PropertyToID("_PupilOffsetY");
+	private IEyeGatherer _eyeGatherer;
+	private IEyeOffsetProvider[] _offsetProviders;
+	static readonly int PUPIL_OFFSET_X_PROP_ID = Shader.PropertyToID("_PupilOffsetX");
+	static readonly int PUPIL_OFFSET_Y_PROP_ID = Shader.PropertyToID("_PupilOffsetY");
+
+	// The pupil stars centered so it can be scaled. Move it to the default eye pos
+	static readonly Vector2 InherentOffset = new Vector2(.094f, 0.151f);
+
+	void Awake()
+	{
+		_eyeGatherer = this.GetComponentInParent<IEyeGatherer>();
+		_offsetProviders = this.GetComponents<IEyeOffsetProvider>().Where(c => c.enabled).ToArray();
+		Update();
+	}
 
 
-    void Awake()
-    {
-        _eyeGatherer = this.GetComponentInParent<IEyeGatherer>();
-        _offsetProviders = this.GetComponents<IEyeOffsetProvider>().Where(c => c.enabled).ToArray();
-    }
+	void Update()
+	{
+		Vector2 combinedOffset = Vector2.zero;
 
+		foreach (var offsetProvider in _offsetProviders)
+		{
+			combinedOffset += offsetProvider.Offset;
+		}
+		var offsetLeft = new Vector2(combinedOffset.x, combinedOffset.y) + InherentOffset;
+		var offsetRight = new Vector2(-combinedOffset.x, combinedOffset.y) + InherentOffset;
+		SetMaterial(_eyeGatherer.EyeMaterials[0], offsetLeft);
+		SetMaterial(_eyeGatherer.EyeMaterials[1], offsetRight);
 
-    void Update()
-    {
-        Vector2 combinedOffset = Vector2.zero;
-        foreach (var offsetProvider in _offsetProviders)
-        {
-            combinedOffset += offsetProvider.Offset;
-        }
-        SetMaterial(_eyeGatherer.EyeMaterials[0], combinedOffset.x, combinedOffset.y);
-        SetMaterial(_eyeGatherer.EyeMaterials[1], -combinedOffset.x, combinedOffset.y);
-
-        void SetMaterial(Material eyeMaterial, float x, float y)
-        {
-            eyeMaterial.SetFloat(PUPIL_OFFSET_X_PROP_ID, x);
-            eyeMaterial.SetFloat(PUPIL_OFFSET_Y_PROP_ID, y);
-        }
-    }
+		void SetMaterial(Material eyeMaterial, Vector2 offset)
+		{
+			eyeMaterial.SetFloat(PUPIL_OFFSET_X_PROP_ID, offset.x);
+			eyeMaterial.SetFloat(PUPIL_OFFSET_Y_PROP_ID, offset.y);
+		}
+	}
 }
