c3b500d961cf84841f4765ae4a7c8821c9a4c68d
Thomas Bartlett <tbartl@umich.edu>
2025-06-01 22:38:03 -0400

    Zoom in on scroll

---
+++ Assets/Scripts/Entities/Character/Creator/Interaction/ZoomInOnScroll.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ZoomInOnScroll : MonoBehaviour
+{
+	[SerializeField] Vector3 _zoomPos;
+	[SerializeField] Vector3 _zoomRot;
+	[SerializeField] float _scrollSensitivity = 1f;
+	[SerializeField] float _lerpPower = 5f;
+
+	Vector3 _startPos;
+	Quaternion _startRot;
+	Quaternion _zoomRotQuaternion;
+	float _targetPercent = 0f;
+	float _currentPercent = 0;
+
+	void Start()
+	{
+		_startPos = transform.localPosition;
+		_startRot = transform.localRotation;
+		_zoomRotQuaternion = Quaternion.Euler(_zoomRot);
+	}
+
+	void Update()
+	{
+		float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+		if (Mathf.Abs(scroll) > 0.0001f)
+		{
+			_targetPercent += scroll * _scrollSensitivity;
+			_targetPercent = Mathf.Clamp01(_targetPercent);
+		}
+
+		_currentPercent = Mathf.Lerp(_currentPercent, _targetPercent, Time.deltaTime * _lerpPower);
+		transform.localPosition = Vector3.Lerp(_startPos, _zoomPos, _currentPercent);
+		transform.localRotation = Quaternion.Lerp(_startRot, _zoomRotQuaternion, _currentPercent);
+	}
+}
