37292c37f716a26c775724e085f314612fad20b2
Thomas Bartlett <tbartl@umich.edu>
2025-06-02 22:21:51 -0400

    Restrict cam tools a bit

--- Assets/Scripts/Entities/Character/Creator/Interaction/SpinYingletOnMouse.cs
+++ Assets/Scripts/Entities/Character/Creator/Interaction/SpinYingletOnMouse.cs
@@ -1,11 +1,21 @@
+using Character.Creator.UI;
 using UnityEngine;
 
 public class SpinYingletOnMouse : MonoBehaviour
 {
 	[SerializeField] float _spinSensitivity = 10f;
+	private IClipboardSelection _clipboardSelection;
+
+	private void Awake()
+	{
+		_clipboardSelection = this.GetCharacterCreatorComponent<IClipboardSelection>();
+	}
 
 	void Update()
 	{
+		// Early return if we're in photo mode
+		if (_clipboardSelection.Selection.Val == ClipboardSelectionType.PhotoMode) return;
+
 		if (Input.GetMouseButton(1))
 		{
 			float spinAmount = Input.GetAxisRaw("Mouse X") * _spinSensitivity;
--- Assets/Scripts/Entities/Character/Creator/Interaction/ZoomInOnScroll.cs
+++ Assets/Scripts/Entities/Character/Creator/Interaction/ZoomInOnScroll.cs
@@ -1,12 +1,15 @@
+using Character.Creator.UI;
 using UnityEngine;
 
 public class ZoomInOnScroll : MonoBehaviour
 {
+	[SerializeField] Hoverable _nonGuiHoverable;
 	[SerializeField] Vector3 _zoomPos;
 	[SerializeField] Vector3 _zoomRot;
 	[SerializeField] float _scrollSensitivity = 1f;
 	[SerializeField] float _lerpPower = 5f;
 
+	IClipboardSelection _clipboardSelection;
 	IYingletHeightProvider _heightProvider;
 	Vector3 _startPos;
 	Quaternion _startRot;
@@ -16,6 +19,7 @@ public class ZoomInOnScroll : MonoBehaviour
 
 	void Start()
 	{
+		_clipboardSelection = this.GetCharacterCreatorComponent<IClipboardSelection>();
 		_heightProvider = this.GetCharacterCreatorComponent<IYingletHeightProvider>();
 		_startPos = transform.localPosition;
 		_startRot = transform.localRotation;
@@ -24,16 +28,31 @@ public class ZoomInOnScroll : MonoBehaviour
 
 	void Update()
 	{
+
+		// Early return if we're in photo mode
+		if (_clipboardSelection.Selection.Val == ClipboardSelectionType.PhotoMode) return;
+
+		UpdateTargetPercent();
+		UpdatePos();
+	}
+	void UpdateTargetPercent()
+	{
+		// Early return if we're hovering over GUI
+		if (!_nonGuiHoverable.Hovered.Val) return;
+
 		float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
 		if (Mathf.Abs(scroll) > 0.0001f)
 		{
 			_targetPercent += scroll * _scrollSensitivity;
 			_targetPercent = Mathf.Clamp01(_targetPercent);
 		}
-
+	}
+	private void UpdatePos()
+	{
 		var zoomOffset = new Vector3(0, _heightProvider.YScale - 1, 0);
 		_currentPercent = Mathf.Lerp(_currentPercent, _targetPercent, Time.deltaTime * _lerpPower);
 		transform.localPosition = Vector3.Lerp(_startPos, _zoomPos + zoomOffset, _currentPercent);
 		transform.localRotation = Quaternion.Lerp(_startRot, _zoomRotQuaternion, _currentPercent);
 	}
+
 }
