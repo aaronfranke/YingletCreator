c06546c4de1a2be05bd3f02c139150112c24a6a7
Thomas Bartlett <tbartl@umich.edu>
2025-06-07 12:08:03 -0400

    Don't show hover circle when hovering over currently editing

--- Assets/Scripts/Entities/Character/Creator/Pose/PoseYingHoverCircle.cs
+++ Assets/Scripts/Entities/Character/Creator/Pose/PoseYingHoverCircle.cs
@@ -2,24 +2,40 @@ using Reactivity;
 
 public class PoseYingHoverCircle : ReactiveBehaviour
 {
+	private IPoseData _poseData;
 	private IHoveredPoseYingProvider _activelyHoveredProvider;
+
+	Computed<bool> _shouldShowThis;
 
 	private void Awake()
 	{
+		_poseData = this.GetComponentInParent<IPoseData>();
 		_activelyHoveredProvider = this.GetComponentInParent<IHoveredPoseYingProvider>();
+		_shouldShowThis = this.CreateComputed(ShouldShowThis);
 		AddReflector(ReflectHovered);
+	}
+
+	private bool ShouldShowThis()
+	{
+		var hovered = _activelyHoveredProvider.HoveredPoseYing;
+		if (hovered == null)
+		{
+			return false;
+		}
+
+		bool hoveringOverEditing = _poseData.CurrentlyEditing?.Reference == hovered.Reference;
+		return !hoveringOverEditing;
 	}
 
 	private void ReflectHovered()
 	{
-		var hovered = _activelyHoveredProvider.HoveredPoseYing;
-		this.gameObject.SetActive(hovered != null);
+		this.gameObject.SetActive(_shouldShowThis.Val);
 	}
 
 	void LateUpdate()
 	{
+		if (!_shouldShowThis.Val) return;
 		var hovered = _activelyHoveredProvider.HoveredPoseYing;
-		if (hovered == null) return;
 		this.transform.position = hovered.GameObject.transform.position;
 		this.transform.localScale = hovered.GameObject.transform.localScale;
 	}
