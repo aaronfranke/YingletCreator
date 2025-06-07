367b6fadea7751ed5dd9a46b044fdae622c2b024
Thomas Bartlett <tbartl@umich.edu>
2025-06-07 12:03:00 -0400

    Better pose ying selection logic; add sound effects

--- Assets/Scripts/Entities/Character/Creator/Pose/PoseYingClicking.cs
+++ Assets/Scripts/Entities/Character/Creator/Pose/PoseYingClicking.cs
@@ -1,26 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Character.Creator.UI
 {
-	internal class PoseYingClicking : MonoBehaviour
+
+	internal interface IPoseYingletClicking
+	{
+		event Action<PoseYing> OnPoseYingChanged;
+	}
+	internal class PoseYingClicking : MonoBehaviour, IPoseYingletClicking
 	{
 		private IPoseData _poseData;
 		private IHoveredPoseYingProvider _hoveredProvider;
+		private IUiHoverManager _uiHoverManager;
+		private IColliderHoverManager _colliderHoverManager;
+
+		public event Action<PoseYing>? OnPoseYingChanged;
 
 		private void Awake()
 		{
 			_poseData = this.GetComponent<IPoseData>();
 			_hoveredProvider = this.GetComponent<IHoveredPoseYingProvider>();
+			_uiHoverManager = Singletons.GetSingleton<IUiHoverManager>();
+			_colliderHoverManager = Singletons.GetSingleton<IColliderHoverManager>();
 		}
 		private void Update()
 		{
 			bool lmbClicked = Input.GetMouseButtonDown(0);
 			if (!lmbClicked) return;
 
-			var currentlyHovered = _hoveredProvider.HoveredPoseYing;
-			if (currentlyHovered == null) return;
+			// Are we hovering over a ying
+			var currentlyHoveredYing = _hoveredProvider.HoveredPoseYing;
+			if (currentlyHoveredYing != null)
+			{
+				if (currentlyHoveredYing.Reference == _poseData.CurrentlyEditing?.Reference)
+				{
+					// Ying is already selected, so let's toggle it off
+					SetEditing(null);
+				}
+				else
+				{
+					// Ying isn't selected, so let's select it
+					SetEditing(currentlyHoveredYing);
+				}
+				return;
+			}
 
-			_poseData.CurrentlyEditing = currentlyHovered;
+			bool hoveringAnything = _uiHoverManager.HoveringUi || _colliderHoverManager.CurrentlyHovered != null;
+			if (!hoveringAnything)
+			{
+				// Hovering over nothing, so let's clear out the selected ying
+				SetEditing(null);
+			}
 		}
+
+		void SetEditing(PoseYing poseYing)
+		{
+			_poseData.CurrentlyEditing = poseYing;
+			OnPoseYingChanged?.Invoke(poseYing);
+		}
+
 	}
 }
