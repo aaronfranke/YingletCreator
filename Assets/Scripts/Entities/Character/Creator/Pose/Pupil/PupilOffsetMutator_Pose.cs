f26adfd9cfcaafdbe4ddf9e46fc7d376e5743a36
Thomas Bartlett <tbartl@umich.edu>
2025-06-18 16:22:47 -0400

    Shift pose ying pupils with UI

---
+++ Assets/Scripts/Entities/Character/Creator/Pose/Pupil/PupilOffsetMutator_Pose.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+internal sealed class PupilOffsetMutator_Pose : MonoBehaviour, IPupilOffsetMutator
+{
+	private IPoseYingDataRepository _dataRepo;
+
+	private void Awake()
+	{
+		_dataRepo = this.GetComponentInParent<IPoseYingDataRepository>();
+	}
+
+	public PupilOffsets Mutate(PupilOffsets input)
+	{
+		return _dataRepo.YingPoseData.PupilData.Val;
+	}
+}
