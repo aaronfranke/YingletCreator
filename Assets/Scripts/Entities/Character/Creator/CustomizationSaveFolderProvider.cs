df82dd754c4224b148a005bb5764e9d9ab427998
Thomas Bartlett <tbartl@umich.edu>
2025-06-19 14:19:54 -0400

    Take picture on left click

--- Assets/Scripts/Entities/Character/Creator/CustomizationSaveFolderProvider.cs
+++ Assets/Scripts/Entities/Character/Creator/CustomizationSaveFolderProvider.cs
@@ -4,47 +4,75 @@ using UnityEngine;
 
 namespace Character.Creator
 {
-    public interface ICustomizationSaveFolderProvider
-    {
-        string PresetFolderRoot { get; }
-        string CustomFolderRoot { get; }
-    }
+	public interface ICustomizationSaveFolderProvider
+	{
+		string PresetFolderRoot { get; }
+		string CustomFolderRoot { get; }
+		string PhotoRoot { get; }
+	}
 
-    public class CustomizationSaveFolderProvider : MonoBehaviour, ICustomizationSaveFolderProvider
-    {
+	public class CustomizationSaveFolderProvider : MonoBehaviour, ICustomizationSaveFolderProvider
+	{
 
-        string _presetFolderRoot;
-        public string PresetFolderRoot
-        {
-            get
-            {
-                if (_presetFolderRoot == null)
-                {
-                    string streamingAssetsPath = Application.streamingAssetsPath;
-                    string folder = Path.Combine(streamingAssetsPath, "PresetYings");
-                    _presetFolderRoot = folder;
-                }
-                return _presetFolderRoot;
-            }
-        }
+		string _presetFolderRoot;
+		public string PresetFolderRoot
+		{
+			get
+			{
+				if (_presetFolderRoot == null)
+				{
+					string streamingAssetsPath = Application.streamingAssetsPath;
+					string folder = Path.Combine(streamingAssetsPath, "PresetYings");
+					_presetFolderRoot = folder;
+				}
+				return _presetFolderRoot;
+			}
+		}
 
-        string _customFolderRoot;
-        public string CustomFolderRoot
-        {
-            get
-            {
-                if (_customFolderRoot == null)
-                {
-                    string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                    string folder = Path.Combine(documentsPath, "My Games", Application.productName, "CustomYings");
-                    if (!Directory.Exists(folder))
-                    {
-                        Directory.CreateDirectory(folder);
-                    }
-                    _customFolderRoot = folder;
-                }
-                return _customFolderRoot;
-            }
-        }
-    }
+		string GameRoot
+		{
+			get
+			{
+				string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+				return Path.Combine(documentsPath, "My Games", Application.productName);
+			}
+
+		}
+
+		string _customFolderRoot;
+		public string CustomFolderRoot
+		{
+			get
+			{
+				if (_customFolderRoot == null)
+				{
+					string folder = Path.Combine(GameRoot, "CustomYings");
+					if (!Directory.Exists(folder))
+					{
+						Directory.CreateDirectory(folder);
+					}
+					_customFolderRoot = folder;
+				}
+				return _customFolderRoot;
+			}
+		}
+
+		string _photoRoot;
+		public string PhotoRoot
+		{
+			get
+			{
+				if (_photoRoot == null)
+				{
+					string folder = Path.Combine(GameRoot, "Photos");
+					if (!Directory.Exists(folder))
+					{
+						Directory.CreateDirectory(folder);
+					}
+					_photoRoot = folder;
+				}
+				return _photoRoot;
+			}
+		}
+	}
 }
---
+++ Assets/Scripts/Entities/Character/Creator/Pose/Picture/TakePictureOnLeftClick.cs
@@ -0,0 +1,86 @@
+using Character.Creator;
+using Character.Creator.UI;
+using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class TakePictureOnLeftClick : MonoBehaviour
+{
+	private ICustomizationSaveFolderProvider _locationProvider;
+	private ICharacterCreatorVisibilityControl _visibilityControl;
+	private Camera _camera;
+
+	private void Start()
+	{
+		_locationProvider = this.GetComponentInParent<ICustomizationSaveFolderProvider>();
+		_visibilityControl = this.GetComponentInParent<ICharacterCreatorVisibilityControl>();
+		_camera = this.GetComponentInChildren<Camera>();
+	}
+
+	private void Update()
+	{
+		// Early return if we're showing UI (aka not in photo mode)
+		if (_visibilityControl.IsVisible.Val) return;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			StartCoroutine(TakePicture());
+		}
+	}
+
+	IEnumerator TakePicture()
+	{
+		yield return new WaitForEndOfFrame();
+		var screenResolution = Screen.currentResolution;
+		var resolution = new Vector2Int(screenResolution.width, screenResolution.height);
+		resolution *= 2; // Experimental resolution doubling
+		RenderTexture rt = new RenderTexture(resolution.x, resolution.y, 24);
+		rt.Create();
+
+		var previousRt = _camera.targetTexture; ;
+		using (var swapper = new CameraTargetSwapper(_camera, rt))
+		{
+			_camera.Render();
+		}
+
+		RenderTexture.active = rt;
+		Texture2D image = new Texture2D(resolution.x, resolution.y, TextureFormat.RGB24, false);
+		image.ReadPixels(new Rect(0, 0, resolution.x, resolution.y), 0, 0);
+		image.Apply();
+		RenderTexture.active = null;
+
+		rt.Release();
+		Destroy(rt);
+
+		SavePicture(image);
+	}
+
+	void SavePicture(Texture2D image)
+	{
+		// Save to desktop
+		byte[] bytes = image.EncodeToPNG();
+		Destroy(image);
+
+		string fileName = $"Picture_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+		string fullPath = Path.Combine(_locationProvider.PhotoRoot, fileName);
+		File.WriteAllBytes(fullPath, bytes);
+	}
+
+	sealed class CameraTargetSwapper : IDisposable
+	{
+		private Camera _camera;
+		private RenderTexture _previousTarget;
+
+		public CameraTargetSwapper(Camera camera, RenderTexture newTarget)
+		{
+			_camera = camera;
+			_previousTarget = camera.targetTexture;
+			_camera.targetTexture = newTarget;
+		}
+		public void Dispose()
+		{
+			_camera.targetTexture = _previousTarget;
+		}
+	}
+}
