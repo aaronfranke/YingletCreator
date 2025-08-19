8372fd4cbe8947d5f616bb2e6d86a8f3e94a1d12
Thomas Bartlett <tbartl@umich.edu>
2025-08-18 23:57:00 -0400

    Actually write values to disk properly

--- Assets/Scripts/GenericUI/Menu/Settings/ISettings/SerializableSettings.cs
+++ Assets/Scripts/GenericUI/Menu/Settings/ISettings/SerializableSettings.cs
@@ -1,10 +1,12 @@
-﻿[System.Serializable]
+﻿using UnityEngine;
+
+[System.Serializable]
 public sealed class SerializableSettings : ISettings
 {
-	FpsCap _fpsCap = FpsCap.Fps120;
+	[SerializeField] FpsCap _fpsCap = FpsCap.Fps120;
 	public FpsCap FpsCap { get => _fpsCap; }
 
-	float _effectVolume = 1.0f;
+	[SerializeField] float _effectVolume = 1.0f;
 	public float EffectVolume => _effectVolume;
 
 	public SerializableSettings() { }
