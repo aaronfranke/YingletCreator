00e84b4ced124bff56784637d212a0c46f080d55
Thomas Bartlett <tbartl@umich.edu>
2025-08-19 23:26:04 -0400

    Add sound effects to dropdown

---
+++ Assets/Scripts/Audio/PlaySoundOnToggleClicked.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySoundOnToggleClicked : MonoBehaviour
+{
+	[SerializeField] private SoundEffect _soundEffect;
+	private IAudioPlayer _audioPlayer;
+	private Toggle _toggle;
+
+	private void Awake()
+	{
+		_audioPlayer = Singletons.GetSingleton<IAudioPlayer>();
+		_toggle = this.GetComponent<Toggle>();
+		_toggle.onValueChanged.AddListener(Toggle_OnValueChanged);
+	}
+
+	private void OnDestroy()
+	{
+		_toggle.onValueChanged.RemoveListener(Toggle_OnValueChanged);
+	}
+
+	private void Toggle_OnValueChanged(bool isOn)
+	{
+		if (!isOn) return;
+		_audioPlayer.Play(_soundEffect);
+	}
+}
