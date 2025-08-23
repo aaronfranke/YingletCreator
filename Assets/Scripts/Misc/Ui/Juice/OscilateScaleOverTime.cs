b7961c388031a16ece74af3dbaf0f308eb57d4b3
Thomas Bartlett <tbartl@umich.edu>
2025-08-22 23:53:46 -0400

    Little juice on the title screen

---
+++ Assets/Scripts/Misc/Ui/Juice/OscilateScaleOverTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+internal class OscillateScaleOverTime : MonoBehaviour
+{
+	public float _amplitude = 0.2f;
+
+	public float _frequency = 2f;
+
+	private Vector3 _initialScale;
+
+	void Start()
+	{
+		_initialScale = this.transform.localScale;
+	}
+
+	void Update()
+	{
+		float scaleFactor = 1 + Mathf.Sin(Time.time * _frequency) * _amplitude;
+		transform.localScale = _initialScale * scaleFactor;
+	}
+}
