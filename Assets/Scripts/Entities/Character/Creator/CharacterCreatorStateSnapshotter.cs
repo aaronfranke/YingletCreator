9d854f7959b47f8a4c385ba0275a1c6c1f749b18
Thomas Bartlett <tbartl@umich.edu>
2025-09-05 16:05:28 -0400

    Functional undo of yinglet selection

--- Assets/Scripts/Entities/Character/Creator/CharacterCreatorStateSnapshotter.cs
+++ Assets/Scripts/Entities/Character/Creator/CharacterCreatorStateSnapshotter.cs
@@ -4,11 +4,15 @@ namespace Character.Creator
 {
 	public sealed class CharacterCreatorStateSnapshot
 	{
-		public CharacterCreatorStateSnapshot(string action)
+		public CharacterCreatorStateSnapshot(string action, CachedYingletReference selected, SerializableCustomizationData serializedData)
 		{
 			Action = action;
+			Selected = selected;
+			SerializedData = serializedData;
 		}
 		public string Action { get; }
+		public CachedYingletReference Selected { get; }
+		public SerializableCustomizationData SerializedData { get; }
 	}
 
 	public interface ICharacterCreatorStateSnapshotter
@@ -20,13 +24,21 @@ namespace Character.Creator
 
 	internal class CharacterCreatorStateSnapshotter : MonoBehaviour, ICharacterCreatorStateSnapshotter
 	{
+		private ICustomizationSelection _selection;
+
+		private void Awake()
+		{
+			_selection = this.GetComponent<ICustomizationSelection>();
+		}
+
 		public CharacterCreatorStateSnapshot GetStateSnapshot(string action)
 		{
-			return new CharacterCreatorStateSnapshot(action);
+			return new CharacterCreatorStateSnapshot(action, _selection.Selected, null);
 		}
 
 		public void RestoreStateSnapshot(CharacterCreatorStateSnapshot snapshot)
 		{
+			_selection.Selected = snapshot.Selected;
 		}
 	}
 }
--- Assets/Scripts/Entities/Character/Creator/UI/MainPage/YingPortraits/YingPortraitClicking.cs
+++ Assets/Scripts/Entities/Character/Creator/UI/MainPage/YingPortraits/YingPortraitClicking.cs
@@ -6,12 +6,14 @@ namespace Character.Creator.UI
 	public class YingPortraitClicking : ReactiveBehaviour
 	{
 		private ICustomizationSelection _selection;
+		private ICharacterCreatorUndoManager _undoManager;
 		private IYingPortraitReference _reference;
 		private Button _button;
 
 		private void Awake()
 		{
 			_selection = this.GetComponentInParent<ICustomizationSelection>();
+			_undoManager = this.GetComponentInParent<ICharacterCreatorUndoManager>();
 			_reference = this.GetComponent<IYingPortraitReference>();
 			_button = this.GetComponent<Button>();
 			_button.onClick.AddListener(Button_OnClick);
@@ -30,6 +32,7 @@ namespace Character.Creator.UI
 
 		private void Button_OnClick()
 		{
+			_undoManager.RecordState($"Selected yinglet: {_reference.Reference.CachedData.Name}");
 			_selection.Selected = _reference.Reference;
 		}
 		void ReflectInteractable()
