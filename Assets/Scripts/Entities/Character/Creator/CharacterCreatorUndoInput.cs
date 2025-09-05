df40637cdcb8cf1210faa433c6f339e69df60eed
Thomas Bartlett <tbartl@umich.edu>
2025-09-05 16:25:36 -0400

    Add pose mode check to undo input

--- Assets/Scripts/Entities/Character/Creator/CharacterCreatorUndoInput.cs
+++ Assets/Scripts/Entities/Character/Creator/CharacterCreatorUndoInput.cs
@@ -1,4 +1,5 @@
 ﻿
+using Character.Creator.UI;
 using UnityEngine;
 
 namespace Character.Creator
@@ -7,15 +8,18 @@ namespace Character.Creator
 	public class CharacterCreatorUndoInput : MonoBehaviour
 	{
 		private ICharacterCreatorUndoManager _undoManager;
+		private IInPoseModeChecker _inPoseMode;
 
 		private void Awake()
 		{
 			_undoManager = GetComponentInParent<ICharacterCreatorUndoManager>();
+			_inPoseMode = this.GetCharacterCreatorComponent<IInPoseModeChecker>();
 		}
 
 		private void Update()
 		{
-			if (!Input.GetKey(KeyCode.LeftControl)) return;
+			if (!Input.GetKey(KeyCode.LeftControl)) return; // Need to hold ctrl
+			if (_inPoseMode.InPoseMode.Val) return; // Need to not be in pose mode
 
 			if (Input.GetKeyDown(KeyCode.Z))
 			{
