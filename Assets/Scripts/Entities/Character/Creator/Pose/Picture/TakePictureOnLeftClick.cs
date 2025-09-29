475a04f35759ee4b7af79662f29fd0d46cbe4e2e
Thomas Bartlett <tbartl@umich.edu>
2025-09-28 21:22:05 -0400

    Move resolution scale to const so it's easier to adjust while gathering media

--- Assets/Scripts/Entities/Character/Creator/Pose/Picture/TakePictureOnLeftClick.cs
+++ Assets/Scripts/Entities/Character/Creator/Pose/Picture/TakePictureOnLeftClick.cs
@@ -7,118 +7,120 @@ using UnityEngine;
 
 public interface ITakePictureEvents
 {
-	event Action<string> PictureTaken;
+    event Action<string> PictureTaken;
 }
 
 public class TakePictureOnLeftClick : MonoBehaviour, ITakePictureEvents
 {
-	private ICustomizationSaveFolderProvider _locationProvider;
-	private IPhotoModeState _photoModeState;
-	private Camera _mainCamera;
+    const int PictureResolutionScale = 2;
 
-	float _lastPicTime = 0;
+    private ICustomizationSaveFolderProvider _locationProvider;
+    private IPhotoModeState _photoModeState;
+    private Camera _mainCamera;
 
-	public event Action<string> PictureTaken;
+    float _lastPicTime = 0;
 
-	private void Start()
-	{
-		_locationProvider = this.GetComponentInParent<ICustomizationSaveFolderProvider>();
-		_photoModeState = this.GetComponentInParent<IPhotoModeState>();
-		_mainCamera = this.GetComponentInChildren<Camera>();
-	}
+    public event Action<string> PictureTaken;
 
-	private void Update()
-	{
-		// Early return if we're showing UI (aka not in photo mode)
-		if (!_photoModeState.IsInPhotoMode.Val) return;
+    private void Start()
+    {
+        _locationProvider = this.GetComponentInParent<ICustomizationSaveFolderProvider>();
+        _photoModeState = this.GetComponentInParent<IPhotoModeState>();
+        _mainCamera = this.GetComponentInChildren<Camera>();
+    }
 
-		// Don't let the user take too many pics
-		if (Time.time < _lastPicTime + 1.5f) return;
+    private void Update()
+    {
+        // Early return if we're showing UI (aka not in photo mode)
+        if (!_photoModeState.IsInPhotoMode.Val) return;
 
-		if (Input.GetMouseButtonDown(0))
-		{
-			StartCoroutine(TakePicture());
-		}
-	}
+        // Don't let the user take too many pics
+        if (Time.time < _lastPicTime + 1.5f) return;
 
-	IEnumerator TakePicture()
-	{
-		// Play the sound effects and stuff immediately
-		var fullPath = GeneratePicturePath();
-		PictureTaken?.Invoke(Path.GetFileName(fullPath));
-		_lastPicTime = Time.time;
-		yield return null;
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(TakePicture());
+        }
+    }
 
-		yield return new WaitForEndOfFrame();
-		var screenResolution = Screen.currentResolution;
-		var resolution = new Vector2Int(screenResolution.width, screenResolution.height);
-		resolution *= 2; // Experimental resolution doubling
-		RenderTexture rt = new RenderTexture(resolution.x, resolution.y, 24, RenderTextureFormat.ARGB32);
-		rt.Create();
+    IEnumerator TakePicture()
+    {
+        // Play the sound effects and stuff immediately
+        var fullPath = GeneratePicturePath();
+        PictureTaken?.Invoke(Path.GetFileName(fullPath));
+        _lastPicTime = Time.time;
+        yield return null;
 
-		// Create a new camera since the main one has a bunch of post-processing junk which messes up the background transparency(?)
-		// Not sure that was actually what was happening, but this seems to work
-		using (var camera = new TemporaryCamera(_mainCamera, rt))
-		{
-			camera.Render();
-		}
+        yield return new WaitForEndOfFrame();
+        var screenResolution = Screen.currentResolution;
+        var resolution = new Vector2Int(screenResolution.width, screenResolution.height);
+        resolution *= PictureResolutionScale;
+        RenderTexture rt = new RenderTexture(resolution.x, resolution.y, 24, RenderTextureFormat.ARGB32);
+        rt.Create();
 
-		RenderTexture.active = rt;
-		Texture2D image = new Texture2D(resolution.x, resolution.y, TextureFormat.RGBA32, false);
-		image.ReadPixels(new Rect(0, 0, resolution.x, resolution.y), 0, 0);
-		image.Apply();
-		RenderTexture.active = null;
+        // Create a new camera since the main one has a bunch of post-processing junk which messes up the background transparency(?)
+        // Not sure that was actually what was happening, but this seems to work
+        using (var camera = new TemporaryCamera(_mainCamera, rt))
+        {
+            camera.Render();
+        }
 
-		rt.Release();
-		Destroy(rt);
+        RenderTexture.active = rt;
+        Texture2D image = new Texture2D(resolution.x, resolution.y, TextureFormat.RGBA32, false);
+        image.ReadPixels(new Rect(0, 0, resolution.x, resolution.y), 0, 0);
+        image.Apply();
+        RenderTexture.active = null;
 
-		SavePicture(image, fullPath);
-	}
+        rt.Release();
+        Destroy(rt);
 
-	string GeneratePicturePath()
-	{
+        SavePicture(image, fullPath);
+    }
 
-		string fileName = $"Picture_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
-		string fullPath = Path.Combine(_locationProvider.PhotoRoot, fileName);
-		return fullPath;
-	}
+    string GeneratePicturePath()
+    {
 
-	void SavePicture(Texture2D image, string fullPath)
-	{
-		// Save to desktop
-		byte[] bytes = image.EncodeToPNG();
-		Destroy(image);
+        string fileName = $"Picture_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+        string fullPath = Path.Combine(_locationProvider.PhotoRoot, fileName);
+        return fullPath;
+    }
 
-		File.WriteAllBytes(fullPath, bytes);
-	}
+    void SavePicture(Texture2D image, string fullPath)
+    {
+        // Save to desktop
+        byte[] bytes = image.EncodeToPNG();
+        Destroy(image);
 
-	sealed class TemporaryCamera : IDisposable
-	{
-		private readonly GameObject _cameraGo;
-		private readonly Camera _camera;
+        File.WriteAllBytes(fullPath, bytes);
+    }
 
-		public TemporaryCamera(Camera mainCamera, RenderTexture rt)
-		{
-			_cameraGo = new GameObject("SnapshotterCamera");
-			_camera = _cameraGo.AddComponent<Camera>();
-			_camera.transform.position = mainCamera.transform.position;
-			_camera.transform.rotation = mainCamera.transform.rotation;
-			_camera.cullingMask = mainCamera.cullingMask;
-			_camera.nearClipPlane = mainCamera.nearClipPlane;
-			_camera.fieldOfView = mainCamera.fieldOfView;
-			_camera.clearFlags = CameraClearFlags.Color;
-			_camera.backgroundColor = Color.clear; // Fully transparent
-			_camera.targetTexture = rt;
-		}
-		public void Render()
-		{
-			_camera.Render();
-		}
-		public void Dispose()
-		{
-			Destroy(_cameraGo);
-		}
+    sealed class TemporaryCamera : IDisposable
+    {
+        private readonly GameObject _cameraGo;
+        private readonly Camera _camera;
 
-	}
+        public TemporaryCamera(Camera mainCamera, RenderTexture rt)
+        {
+            _cameraGo = new GameObject("SnapshotterCamera");
+            _camera = _cameraGo.AddComponent<Camera>();
+            _camera.transform.position = mainCamera.transform.position;
+            _camera.transform.rotation = mainCamera.transform.rotation;
+            _camera.cullingMask = mainCamera.cullingMask;
+            _camera.nearClipPlane = mainCamera.nearClipPlane;
+            _camera.fieldOfView = mainCamera.fieldOfView;
+            _camera.clearFlags = CameraClearFlags.Color;
+            _camera.backgroundColor = Color.clear; // Fully transparent
+            _camera.targetTexture = rt;
+        }
+        public void Render()
+        {
+            _camera.Render();
+        }
+        public void Dispose()
+        {
+            Destroy(_cameraGo);
+        }
+
+    }
 
 }
