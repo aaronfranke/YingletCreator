48afc203b46fdae0e5b764fe07809d6ec00b60f0
Thomas Bartlett <tbartl@umich.edu>
2025-10-04 19:08:50 -0400

    Fix discord link

--- Assets/Scripts/GenericUI/Menu/OpenDiscordOnClick.cs
+++ Assets/Scripts/GenericUI/Menu/OpenDiscordOnClick.cs
@@ -3,23 +3,23 @@ using UnityEngine.UI;
 
 public class OpenDiscordOnClick : MonoBehaviour
 {
-	const string DiscordUrl = "https://www.discord.gg/yinglet";
+    const string DiscordUrl = "https://www.discord.gg/oops-all-yinglets";
 
-	private Button _button;
+    private Button _button;
 
-	void Awake()
-	{
-		_button = this.GetComponent<Button>();
-		_button.onClick.AddListener(Button_OnClick);
-	}
+    void Awake()
+    {
+        _button = this.GetComponent<Button>();
+        _button.onClick.AddListener(Button_OnClick);
+    }
 
-	private void OnDestroy()
-	{
-		_button.onClick.RemoveListener(Button_OnClick);
-	}
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(Button_OnClick);
+    }
 
-	private void Button_OnClick()
-	{
-		Application.OpenURL(DiscordUrl);
-	}
+    private void Button_OnClick()
+    {
+        Application.OpenURL(DiscordUrl);
+    }
 }
