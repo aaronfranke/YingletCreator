0c4cecd304a4ce360bf4c646d9ac4878a47ba3aa
Thomas Bartlett <tbartl@umich.edu>
2025-10-10 22:19:16 -0400

    Boop mouth expressions

---
+++ Assets/Scripts/Entities/Animation/Booped/MouthExpressionsMutator_Booped.cs
@@ -0,0 +1,47 @@
+using Reactivity;
+using System.Collections;
+using UnityEngine;
+
+public class MouthExpressionsMutator_Booped : MonoBehaviour, IMouthExpressionsMutator
+{
+    [SerializeField] float WaitTime = 2f;
+
+    private IBoopManager _boopManager;
+    Observable<bool> _isBooped = new();
+    private Coroutine _boopCoroutine;
+
+    void Awake()
+    {
+        _boopManager = this.GetComponentInParent<IBoopManager>();
+        _boopManager.OnBoop += OnBooped;
+    }
+
+    void OnDestroy()
+    {
+        _boopManager.OnBoop -= OnBooped;
+    }
+    private void OnEnable()
+    {
+        _isBooped.Val = false;
+    }
+
+    private void OnBooped()
+    {
+        this.StopAndStartCoroutine(ref _boopCoroutine, Booped());
+        ;
+    }
+
+    IEnumerator Booped()
+    {
+        _isBooped.Val = true;
+        yield return new WaitForSeconds(WaitTime);
+        _isBooped.Val = false;
+    }
+    public void Mutate(ref MouthExpression expression, ref MouthOpenAmount openAmount)
+    {
+        if (!_isBooped.Val) return;
+
+        expression = MouthExpression.Muse;
+        openAmount = MouthOpenAmount.Closed;
+    }
+}
--- Assets/Scripts/Entities/Character/Creator/Pose/Expressions/MouthExpressionsMutator_Pose.cs
+++ Assets/Scripts/Entities/Character/Creator/Pose/Expressions/MouthExpressionsMutator_Pose.cs
@@ -2,37 +2,37 @@ using Reactivity;
 
 public class MouthExpressionsMutator_Pose : ReactiveBehaviour, IMouthExpressionsMutator
 {
-	private IPoseYingDataRepository _dataRepo;
-	Computed<int> _intValueComputed;
-	Computed<MouthExpression> _defaultExpressionComputed;
-	Computed<MouthOpenAmount> _defaultOpenAmountComputed;
+    private IPoseYingDataRepository _dataRepo;
+    Computed<int> _intValueComputed;
+    Computed<MouthExpression> _defaultExpressionComputed;
+    Computed<MouthOpenAmount> _defaultOpenAmountComputed;
 
-	void Awake()
-	{
-		_dataRepo = this.GetComponentInParent<IPoseYingDataRepository>();
-		_intValueComputed = CreateComputed(ComputeDefaultIntValue);
-		_defaultExpressionComputed = CreateComputed(ComputeDefaultExpression);
-		_defaultOpenAmountComputed = CreateComputed(ComputeDefaultOpenAmount);
-	}
+    void Awake()
+    {
+        _dataRepo = this.GetComponentInParent<IPoseYingDataRepository>();
+        _intValueComputed = CreateComputed(ComputeDefaultIntValue);
+        _defaultExpressionComputed = CreateComputed(ComputeDefaultExpression);
+        _defaultOpenAmountComputed = CreateComputed(ComputeDefaultOpenAmount);
+    }
 
-	private int ComputeDefaultIntValue()
-	{
-		return _dataRepo.YingPoseData.MouthExpressionNum;
-	}
+    private int ComputeDefaultIntValue()
+    {
+        return _dataRepo.YingPoseData.MouthExpressionNum;
+    }
 
-	private MouthExpression ComputeDefaultExpression()
-	{
-		return MouthExpressionsMutator_Default.GetExpressionFromInt(_intValueComputed.Val);
-	}
+    private MouthExpression ComputeDefaultExpression()
+    {
+        return MouthExpressionsMutator_Default.GetExpressionFromInt(_intValueComputed.Val);
+    }
 
-	private MouthOpenAmount ComputeDefaultOpenAmount()
-	{
-		return MouthExpressionsMutator_Default.GetOpenAmountFromInt(_intValueComputed.Val);
-	}
+    private MouthOpenAmount ComputeDefaultOpenAmount()
+    {
+        return MouthExpressionsMutator_Default.GetOpenAmountFromInt(_intValueComputed.Val);
+    }
 
-	public void Mutate(ref MouthExpression expression, ref MouthOpenAmount openAmount)
-	{
-		expression = _defaultExpressionComputed.Val;
-		openAmount = _defaultOpenAmountComputed.Val;
-	}
+    public void Mutate(ref MouthExpression expression, ref MouthOpenAmount openAmount)
+    {
+        expression = _defaultExpressionComputed.Val;
+        openAmount = _defaultOpenAmountComputed.Val;
+    }
 }
