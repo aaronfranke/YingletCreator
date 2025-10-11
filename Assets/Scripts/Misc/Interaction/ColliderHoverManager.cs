79863843a1273ba0e6a14b264d6f4cb93e474f71
Thomas Bartlett <tbartl@umich.edu>
2025-10-11 10:29:50 -0400

    Fix collider ordering

--- Assets/Scripts/Misc/Interaction/ColliderHoverManager.cs
+++ Assets/Scripts/Misc/Interaction/ColliderHoverManager.cs
@@ -5,80 +5,81 @@ using UnityEngine;
 
 public interface IColliderHoverManager
 {
-	IColliderHoverable CurrentlyHovered { get; }
+    IColliderHoverable CurrentlyHovered { get; }
 
-	IDisposable DisableHovering();
+    IDisposable DisableHovering();
 }
 
 public class ColliderHoverManager : MonoBehaviour, IColliderHoverManager
 {
-	const float MaxRaycastDistance = 50f;
+    const float MaxRaycastDistance = 50f;
 
-	Observable<IColliderHoverable> _currentlyHovered = new();
-	private IUiHoverManager _uiHoverManager;
-	bool _disabled = false;
+    Observable<IColliderHoverable> _currentlyHovered = new();
+    private IUiHoverManager _uiHoverManager;
+    bool _disabled = false;
 
-	public IColliderHoverable CurrentlyHovered => _currentlyHovered.Val;
+    public IColliderHoverable CurrentlyHovered => _currentlyHovered.Val;
 
-	void Awake()
-	{
-		_uiHoverManager = Singletons.GetSingleton<IUiHoverManager>();
-	}
+    void Awake()
+    {
+        _uiHoverManager = Singletons.GetSingleton<IUiHoverManager>();
+    }
 
-	private void Update()
-	{
-		_currentlyHovered.Val = CalculateColliderHoverable();
-	}
+    private void Update()
+    {
+        _currentlyHovered.Val = CalculateColliderHoverable();
+    }
 
-	IColliderHoverable CalculateColliderHoverable()
-	{
-		if (_disabled)
-		{
-			return null;
-		}
+    IColliderHoverable CalculateColliderHoverable()
+    {
+        if (_disabled)
+        {
+            return null;
+        }
 
-		if (_uiHoverManager.HoveringUi)
-		{
-			// If we're hovering UI, we shouldn't be hovering colliders
-			return null;
-		}
-		// Ray from camera through mouse position
-		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (_uiHoverManager.HoveringUi)
+        {
+            // If we're hovering UI, we shouldn't be hovering colliders
+            return null;
+        }
+        // Ray from camera through mouse position
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		var maxDistance = GetMaxDistance(ray);
+        var maxDistance = GetMaxDistance(ray);
 
-		var itemsHit = Physics.RaycastAll(ray, maxDistance);
-		var firstHoverableHit = itemsHit
-			.Select(itemsHit => itemsHit.collider.GetComponentInParent<IColliderHoverable>())
-			.Where(i => i != null)
-			.Distinct()
-			.OrderByDescending(i => i.PriorityFudge)
-			.FirstOrDefault();
+        var itemsHit = Physics.RaycastAll(ray, maxDistance);
+        var firstHoverableHit = itemsHit
+            .OrderBy(h => h.distance) // The order returned is undefined. Sort by distance first
+            .Select(itemsHit => itemsHit.collider.GetComponentInParent<IColliderHoverable>())
+            .Where(i => i != null)
+            .Distinct()
+            .OrderByDescending(i => i.PriorityFudge) // Also sort by priority fudge. This mantains order if equal
+            .FirstOrDefault();
 
-		return firstHoverableHit;
-	}
+        return firstHoverableHit;
+    }
 
-	float GetMaxDistance(Ray ray)
-	{
-		// Find intersection with XZ plane (y=0)
-		float tPlane = 0f;
-		if (Mathf.Abs(ray.direction.y) > 1e-5f)
-			tPlane = -ray.origin.y / ray.direction.y;
-		else
-			tPlane = float.PositiveInfinity;
+    float GetMaxDistance(Ray ray)
+    {
+        // Find intersection with XZ plane (y=0)
+        float tPlane = 0f;
+        if (Mathf.Abs(ray.direction.y) > 1e-5f)
+            tPlane = -ray.origin.y / ray.direction.y;
+        else
+            tPlane = float.PositiveInfinity;
 
-		// Clamp max distance to 50 units or intersection with XZ plane
-		float maxDistance = Mathf.Min(MaxRaycastDistance, tPlane > 0 ? tPlane : MaxRaycastDistance);
+        // Clamp max distance to 50 units or intersection with XZ plane
+        float maxDistance = Mathf.Min(MaxRaycastDistance, tPlane > 0 ? tPlane : MaxRaycastDistance);
 
-		return maxDistance;
-	}
+        return maxDistance;
+    }
 
-	public IDisposable DisableHovering()
-	{
-		_disabled = true;
-		return new BasicActionDisposable(() =>
-		{
-			_disabled = false;
-		});
-	}
+    public IDisposable DisableHovering()
+    {
+        _disabled = true;
+        return new BasicActionDisposable(() =>
+        {
+            _disabled = false;
+        });
+    }
 }
