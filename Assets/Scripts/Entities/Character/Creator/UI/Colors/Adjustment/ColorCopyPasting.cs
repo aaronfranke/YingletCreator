36c84d6ec62c8c0a49297f7fb867903497426865
Thomas Bartlett <tbartl@umich.edu>
2025-10-16 23:22:21 -0400

    Undo now supports color copy pasting

--- Assets/Scripts/Entities/Character/Creator/UI/Colors/Adjustment/ColorCopyPasting.cs
+++ Assets/Scripts/Entities/Character/Creator/UI/Colors/Adjustment/ColorCopyPasting.cs
@@ -6,115 +6,119 @@ using UnityEngine;
 
 namespace Character.Creator.UI
 {
-	public interface IColorCopyPasting
-	{
-		void Copy();
-		void Paste();
-		event Action Copied;
-		event Action Pasted;
-		event Action PasteFailedInvalidFormat;
-	}
-	public class ColorCopyPasting : MonoBehaviour, IColorCopyPasting
-	{
-		private ICustomizationSelectedDataRepository _dataRepository;
-		private IColorActiveSelection _activeSelection;
+    public interface IColorCopyPasting
+    {
+        void Copy();
+        void Paste();
+        event Action Copied;
+        event Action Pasted;
+        event Action PasteFailedInvalidFormat;
+    }
+    public class ColorCopyPasting : MonoBehaviour, IColorCopyPasting
+    {
+        private ICustomizationSelectedDataRepository _dataRepository;
+        private ICharacterCreatorUndoManager _undoManager;
+        private IColorActiveSelection _activeSelection;
 
-		public event Action Copied = delegate { };
-		public event Action Pasted = delegate { };
-		public event Action PasteFailedInvalidFormat = delegate { };
+        public event Action Copied = delegate { };
+        public event Action Pasted = delegate { };
+        public event Action PasteFailedInvalidFormat = delegate { };
 
-		void Awake()
-		{
-			_dataRepository = GetComponentInParent<ICustomizationSelectedDataRepository>();
-			_activeSelection = this.GetComponent<IColorActiveSelection>();
-		}
-		void Update()
-		{
-			if (!Input.GetKey(KeyCode.LeftControl)) return;
-			if (Input.GetKeyDown(KeyCode.C))
-			{
-				Copy();
-			}
-			if (Input.GetKeyDown(KeyCode.V))
-			{
-				Paste();
-			}
+        void Awake()
+        {
+            _dataRepository = GetComponentInParent<ICustomizationSelectedDataRepository>();
+            _undoManager = this.GetComponentInParent<ICharacterCreatorUndoManager>();
+            _activeSelection = this.GetComponent<IColorActiveSelection>();
+        }
+        void Update()
+        {
+            if (!Input.GetKey(KeyCode.LeftControl)) return;
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                Copy();
+            }
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                Paste();
+            }
 
-		}
-		public void Copy()
-		{
-			var id = _activeSelection.FirstSelected;
-			if (!id) return;
+        }
+        public void Copy()
+        {
+            var id = _activeSelection.FirstSelected;
+            if (!id) return;
 
-			var baseHex = GetHexString(_dataRepository.GetColorizeValues(id).Base);
-			var shadeHex = GetHexString(_dataRepository.GetColorizeValues(id).Shade);
-			GUIUtility.systemCopyBuffer = $"{baseHex} {shadeHex}";
+            var baseHex = GetHexString(_dataRepository.GetColorizeValues(id).Base);
+            var shadeHex = GetHexString(_dataRepository.GetColorizeValues(id).Shade);
+            GUIUtility.systemCopyBuffer = $"{baseHex} {shadeHex}";
 
-			Copied();
-		}
+            Copied();
+        }
 
-		static string GetHexString(IColorizeValuesPart part)
-		{
-			return "#" + ColorUtility.ToHtmlStringRGB(part.GetColor());
-		}
+        static string GetHexString(IColorizeValuesPart part)
+        {
+            return "#" + ColorUtility.ToHtmlStringRGB(part.GetColor());
+        }
 
-		public void Paste()
-		{
-			var (baseColor, shadeColor) = GetColorsFromClipboard();
-			if (baseColor == null)
-			{
-				PasteFailedInvalidFormat();
-				return;
-			}
+        public void Paste()
+        {
+            var (baseColor, shadeColor) = GetColorsFromClipboard();
+            if (baseColor == null)
+            {
+                PasteFailedInvalidFormat();
+                return;
+            }
 
-			var ids = _activeSelection.AllSelected.ToList();
-			using var suspender = new ReactivitySuspender();
-			foreach (var id in ids)
-			{
-				var existingColor = _dataRepository.GetColorizeValues(id);
+            _undoManager.RecordState("Pasted color");
 
-				var newColorizeValues = shadeColor == null
-					? new ColorizeValuesFromRealColor(baseColor.Value, existingColor)
-					: new ColorizeValuesFromRealColor(baseColor.Value, shadeColor.Value);
-				_dataRepository.SetColorizeValues(id, newColorizeValues);
-			}
-			Pasted();
-		}
+            var ids = _activeSelection.AllSelected.ToList();
+            using var suspender = new ReactivitySuspender();
+            foreach (var id in ids)
+            {
+                var existingColor = _dataRepository.GetColorizeValues(id);
 
-		static (Color?, Color?) GetColorsFromClipboard()
-		{
-			string clipboardText = GUIUtility.systemCopyBuffer;
+                var newColorizeValues = shadeColor == null
+                    ? new ColorizeValuesFromRealColor(baseColor.Value, existingColor)
+                    : new ColorizeValuesFromRealColor(baseColor.Value, shadeColor.Value);
+                _dataRepository.SetColorizeValues(id, newColorizeValues);
+            }
+            Pasted();
+        }
 
-			if (string.IsNullOrWhiteSpace(clipboardText)) return (null, null);
+        static (Color?, Color?) GetColorsFromClipboard()
+        {
+            string clipboardText = GUIUtility.systemCopyBuffer;
 
-			var colorParts = clipboardText.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            if (string.IsNullOrWhiteSpace(clipboardText)) return (null, null);
 
-			if (colorParts.Length == 1)
-			{
-				// Single color format
-				var color = ParseSingleColor(colorParts[0]);
-				return (color, null);
-			}
-			else if (colorParts.Length == 2)
-			{
-				// Two color format
-				var colorBase = ParseSingleColor(colorParts[0]);
-				var colorShade = ParseSingleColor(colorParts[1]);
-				return (colorBase, colorShade);
-			}
+            var colorParts = clipboardText.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-			return (null, null);
-		}
+            if (colorParts.Length == 1)
+            {
+                // Single color format
+                var color = ParseSingleColor(colorParts[0]);
+                return (color, null);
+            }
+            else if (colorParts.Length == 2)
+            {
+                // Two color format
+                var colorBase = ParseSingleColor(colorParts[0]);
+                var colorShade = ParseSingleColor(colorParts[1]);
+                return (colorBase, colorShade);
+            }
 
-		static Color? ParseSingleColor(string colorString)
-		{
-			if (string.IsNullOrWhiteSpace(colorString)) return null;
+            return (null, null);
+        }
 
-			string normalizedColor = colorString.Trim();
-			if (!normalizedColor.StartsWith("#"))
-				normalizedColor = "#" + normalizedColor;
+        static Color? ParseSingleColor(string colorString)
+        {
+            if (string.IsNullOrWhiteSpace(colorString)) return null;
 
-			return ColorUtility.TryParseHtmlString(normalizedColor, out Color color) ? color : null;
-		}
-	}
+            string normalizedColor = colorString.Trim();
+            if (!normalizedColor.StartsWith("#"))
+                normalizedColor = "#" + normalizedColor;
+
+            return ColorUtility.TryParseHtmlString(normalizedColor, out Color color) ? color : null;
+        }
+    }
 }
