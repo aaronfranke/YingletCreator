ca06add1e19cae5096b1115e01d9e434be5c3bd7
Thomas Bartlett <tbartl@umich.edu>
2025-10-16 23:15:58 -0400

    Increase default music volume a bit

--- Assets/Scripts/GenericUI/Menu/Settings/ISettings/SerializableSettings.cs
+++ Assets/Scripts/GenericUI/Menu/Settings/ISettings/SerializableSettings.cs
@@ -15,7 +15,7 @@ public sealed class SerializableSettings : ISettings
     [SerializeField] float _effectVolume = 1.0f;
     public float EffectVolume => _effectVolume;
 
-    [SerializeField] float _musicVolume = 0.35f;
+    [SerializeField] float _musicVolume = 0.42f;
     public float MusicVolume => _musicVolume;
 
     public SerializableSettings()
