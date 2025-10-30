5d926f8142d3c5fda54d09ab179d27ee1e44e5a6
Thomas Bartlett <tbartl@umich.edu>
2025-10-29 22:04:44 -0400

    Addressable: CharacterBone

--- Assets/Scripts/Entities/Character/Compositor/ScriptableObjects/MeshWithMaterial.cs
+++ Assets/Scripts/Entities/Character/Compositor/ScriptableObjects/MeshWithMaterial.cs
@@ -11,11 +11,11 @@ namespace Character.Compositor
 		[SerializeField] GameObject _skinnedMeshRendererPrefab;
 		[SerializeField] AssetReferenceT<MaterialDescription> _materialWithDescriptionReference;
 		[SerializeField] CharacterElementTag[] _tags;  // TTODO
-		[SerializeField] CharacterBone _boneToAttachTo;  // TTODO
+		[SerializeField] AssetReferenceT<CharacterBone> _boneToAttachToReference;
 
 		public GameObject SkinnedMeshRendererPrefab => _skinnedMeshRendererPrefab;
 		public MaterialDescription MaterialDescription => _materialWithDescriptionReference.LoadSync();
 		public IEnumerable<CharacterElementTag> Tags => _tags;
-		public CharacterBone BoneToAttachTo => _boneToAttachTo;
+		public CharacterBone BoneToAttachTo => _boneToAttachToReference.LoadSync();
 	}
 }
