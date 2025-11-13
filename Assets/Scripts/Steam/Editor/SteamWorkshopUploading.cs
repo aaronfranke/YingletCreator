b54e4da2316bcc5748e86347dbf355c736292577
Thomas Bartlett <tbartl@umich.edu>
2025-11-12 22:19:21 -0500

    Upload only the relevant part of the sprite

--- Assets/Scripts/Steam/Editor/SteamWorkshopUploading.cs
+++ Assets/Scripts/Steam/Editor/SteamWorkshopUploading.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
@@ -19,7 +20,6 @@ public static class SteamWorkshopUploading
 			}
 		}
 
-		var iconFullPath = modDefinition.Icon.GetFullAssetPath();
 
 		var existingSteamId = modDefinition.SteamWorkshopId;
 
@@ -29,8 +29,15 @@ public static class SteamWorkshopUploading
 			.WithTitle(modDefinition.Title)
 			.WithDescription(modDefinition.ShortDescription)
 			.WithContent(contentFolder)
-			.WithTag("Presets")
-			.WithPreviewFile(iconFullPath);
+			.WithTag("Presets");
+
+		using var tempIconFolder = new TemporaryFolder();
+		if (modDefinition.Icon != null)
+		{
+			var iconFullPath = modDefinition.Icon.GetFullAssetPath();
+			var soloSprite = ExportSpriteToPNG(modDefinition.Icon, tempIconFolder.Path);
+			publishJob = publishJob.WithPreviewFile(iconFullPath);
+		}
 
 		var publishResult = await publishJob.SubmitAsync();
 
@@ -48,5 +55,51 @@ public static class SteamWorkshopUploading
 			// If you're getting InvalidParam like I was, you might need to delete the vdf files out of C:\Program Files (x86)\Steam\appcache and restart steam
 			throw new System.Exception($"Failed to publish to SteamWorks: {publishResult.Result.ToString()}");
 		}
+	}
+
+
+	// We can't provide the sprite alone since there might be other stuff; put it in a temp folder
+	public static string ExportSpriteToPNG(Sprite sprite, string outputFolder)
+	{
+		string assetPath = AssetDatabase.GetAssetPath(sprite.texture);
+		var importer = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+		bool wasReadable = false;
+		if (importer != null)
+		{
+			wasReadable = importer.isReadable;
+			if (!importer.isReadable)
+			{
+				importer.isReadable = true;
+				importer.SaveAndReimport();
+			}
+		}
+		var rect = sprite.rect;
+		var texture = sprite.texture;
+
+		// Crop texture to sprite rect
+		var pixels = texture.GetPixels(
+			(int)rect.x,
+			(int)rect.y,
+			(int)rect.width,
+			(int)rect.height
+		);
+
+		var newTex = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGBA32, false);
+		newTex.SetPixels(pixels);
+		newTex.Apply();
+
+		string filePath = Path.Combine(outputFolder, sprite.name + ".png");
+		byte[] pngData = newTex.EncodeToPNG();
+
+		File.WriteAllBytes(filePath, pngData);
+		Object.DestroyImmediate(newTex);
+
+		if (wasReadable != importer.isReadable)
+		{
+			importer.isReadable = wasReadable;
+			importer.SaveAndReimport();
+		}
+
+		return filePath;
 	}
 }
